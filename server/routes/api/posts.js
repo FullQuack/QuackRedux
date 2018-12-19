75d8b0d5b61ff8c4fdbbc604ed9e476f1ec69310
Jonah Wilkof <jonahwilkof@Jonahs-MacBook-Pro.local>
2018-12-19 10:58:01 -0500

    posts.js

--- server/routes/api/posts.js
+++ server/routes/api/posts.js
@@ -4,6 +4,7 @@ const mongoose = require('mongoose');
 const passport = require('passport');
 
 // Post model
+
 const Post = require('../../models/Post');
 // User model
 const User = require('../../models/User');

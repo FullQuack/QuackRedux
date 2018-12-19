d75a6659114f97c250a45caeb330eeb1488b209c
Jonah Wilkof <jonahwilkof@Jonahs-MacBook-Pro.local>
2018-12-19 10:42:26 -0500

    profile.js

--- server/routes/api/profile.js
+++ server/routes/api/profile.js
@@ -14,9 +14,9 @@ const Profile = require('../../models/Profile');
 // Load Profile Model
 const User = require('../../models/User');
 
-// @route   GET api/profile/test
-// @desc    Tests profile route
-// @access  Public
+// @route    GET api/profile/test
+// @desc     Tests profile route
+// @access   Public
 
 router.get('/test', (req, res) => res.json({
   msg: 'Profile works'
